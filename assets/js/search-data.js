061b274fa4c16f53b592d488ad2ef47c4e505a91
BhargaviParthasarathy <bhargavi1696@gmail.com>
2026-02-16 06:28:00 +0000

    Deploying to gh-pages from @ BhargaviParthasarathy/bhargaviparthasarathy.github.io@9e5291b2b44109b98c23447c0d8dd5afea2fb7d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/travel/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
